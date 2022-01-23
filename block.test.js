2d0938714252fce6a1a87b3d75731ec3de9e7e10
Jamal Williams <me@jamalwilliams.dev>
2022-01-23 16:29:42 -0600

    add mineBlock method and tests for mineblock

--- block.test.js
+++ block.test.js
@@ -40,5 +40,29 @@ describe('genesis()', () => {
     it('returns the genesis data', () => {
         expect(genesisBlock).toEqual(GENESIS_DATA);
     })
+})
 
+describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const minedBlock = Block.mineBlock({lastBlock, data});
+
+    console.log(Date.now())
+
+    it('returns a Block instance', () => {
+        expect(minedBlock instanceof Block).toBe(true);
+    })
+
+    it('sets `lastHash` to be the `hash` of the lastBlock', () => {
+        expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    })
+
+    it('sets  the `data`', () => {
+        expect(minedBlock.data).toEqual(data);
+    }) 
+
+    it('sets  the `timestamp', () => {
+        expect(minedBlock.data).not.toEqual(undefined);
+    })
+    
 })
